8cfa4379b685a7d754219c812848ffea04432778
贾少英 <jiashaoying@163.com>
2021-03-30 17:00:06 +0800

    修订

--- web-share/share/topic/detail/topic_detail.js
+++ web-share/share/topic/detail/topic_detail.js
@@ -1,60 +1,80 @@
 window.onload = function () {
-  var ua = navigator.userAgent.toLowerCase();
-  if (ua.match(/MicroMessenger/i) == "micromessenger") {
-    var DL = document.getElementById("downloadButton");
+  var ua = navigator.userAgent.toLowerCase()
+  if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+    var DL = document.getElementById('downloadButton')
 
     if (ua.indexOf('android') > -1) {
-      DL.href = "http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone";
-      DL.target = "_blank";
+      DL.href =
+        'http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone'
+      DL.target = '_blank'
     } else if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1) {
-      DL.href = "http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone";
-      DL.target = "_blank";
+      DL.href =
+        'http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone'
+      DL.target = '_blank'
     }
   } else {
-
-    var DL = document.getElementById("downloadButton");
+    var DL = document.getElementById('downloadButton')
     if (ua.indexOf('android') > -1) {
-      DL.href = "http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone";
-      DL.target = "_blank";
+      DL.href =
+        'http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone'
+      DL.target = '_blank'
     } else if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1) {
-      DL.href = "itms-apps://itunes.apple.com/cn/app/id1329190498";
-      DL.target = "_blank";
+      DL.href = 'itms-apps://itunes.apple.com/cn/app/id1329190498'
+      DL.target = '_blank'
     }
   }
 }
 $(function () {
   function formatDate(timestamp) {
-    var date = new Date(parseInt(timestamp*1000))
+    var date = new Date(parseInt(timestamp * 1000))
     var year = date.getFullYear()
     var month = date.getMonth() + 1
     var day = date.getDate()
     var hour = date.getHours()
     var minute = date.getMinutes()
     var second = date.getSeconds()
-    return year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second
+    return (
+      year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second
+    )
   }
 
-  function getQueryVariable(variable)
-  {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-      var pair = vars[i].split("=");
-      if(pair[0] == variable){return pair[1];}
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1)
+    var vars = query.split('&')
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=')
+      if (pair[0] == variable) {
+        return pair[1]
+      }
     }
-    return(false);
+    return false
   }
 
-  var APP_DOWN_URL = 'http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone'
-  $("#downloadButton").click(function () {
-    window.location.href = APP_DOWN_URL
-  });
+  var APP_DOWN_URL =
+    'http://a.app.qq.com/o/simple.jsp?pkgname=shengang.szkingdom.android.phone'
+  var scheme = 'community://'
+  $('#downloadButton').click(function () {
+    // var ifr = document.createElement('iframe');
+    // ifr.src = scheme;
+    // ifr.style.display = 'none';
+    // document.body.appendChild(ifr);
+    // window.setTimeout(function () {
+    //    document.body.removeChild(ifr);
+    //    window.location.href = APP_DOWN_URL
+    // }, 300);
+
+    window.location.href = scheme
+    window.setTimeout(function () {
+       window.location.href = APP_DOWN_URL
+    }, 3000);
+  })
 
   requestTopicDetailWithID()
 
   function requestTopicDetailWithID() {
     const topicID = getQueryVariable('topic_id')
-    var url = `http://106.14.166.235/api/v1/mobile/topic/get_detail?topic_id=${topicID}&filter=1&cursor=0`
+    var url = `http://www.neighborsay.com/api/v1/mobile/topic/get_detail?topic_id=${topicID}&filter=1&cursor=0`
+    console.log(url)
     $.get(url, function (result) {
       console.log(result)
       if (result.code > 0) {
@@ -67,11 +87,18 @@ $(function () {
     console.log(data)
     const author = data.author
     $('#topic-title').html(`${data.title}`)
-    $('#owner-avatar').find('img').attr('src', `http://x-community.oss-cn-shanghai.aliyuncs.com/image/202010/fbfb5447cac0d392ef0c8c545eb29c00.jpg`)
-    var content = `<p>${data.content}</p>`;
+    $('#owner-avatar')
+      .find('img')
+      .attr(
+        'src',
+        `http://x-community.oss-cn-shanghai.aliyuncs.com/image/202010/fbfb5447cac0d392ef0c8c545eb29c00.jpg`
+      )
+    var content = `<p>${data.content}</p>`
     $('#bbs-thread-content').html(content)
     $('#owner-name').html(author.username)
-    var time_label = `${formatDate(data.update_at)}<span>评论 ${data.comment_count}</span>`
+    var time_label = `${formatDate(data.update_at)}<span>评论 ${
+      data.comment_count
+    }</span>`
     $('#pub-time').html(time_label)
     let commentsHtml = ''
     $.each(data.comments, function () {
@@ -86,11 +113,14 @@ $(function () {
       commentsHtml += '</div>'
       commentsHtml += `<div class="discuss-light"><span class="light-icon"></span>亮了(${this.like_count})</div>`
       commentsHtml += '</div>'
-      commentsHtml += '<div class="hp-m-discuss-item-wrap-content"><div class="hp-m-discuss-item-wrap-content-container">'
+      commentsHtml +=
+        '<div class="hp-m-discuss-item-wrap-content"><div class="hp-m-discuss-item-wrap-content-container">'
       commentsHtml += `<p>${this.content}</p>`
       commentsHtml += '</div></div>'
       commentsHtml += '<div class="hp-m-discuss-item-wrap-footer">'
-      commentsHtml += `<div class="discuss-time">${formatDate(this.create_at)}</div>`
+      commentsHtml += `<div class="discuss-time">${formatDate(
+        this.create_at
+      )}</div>`
       // commentsHtml += `<div class="watch-discuss">查看评论(11)</div>`
       commentsHtml += '</div>'
       commentsHtml += `</div>`
@@ -98,4 +128,4 @@ $(function () {
     })
     $('#replyAreaLight').append(commentsHtml)
   }
-});
+})
